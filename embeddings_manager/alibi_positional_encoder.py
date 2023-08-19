76167f9c1dad2b7e324e297a49660cd2140e0d1f
afmjoaa <32086430+afmjoaa@users.noreply.github.com>
2023-08-19 21:35:32 +0600

    #2: Complete ALiBiBi encoder

--- embeddings_manager/alibi_positional_encoder.py
+++ embeddings_manager/alibi_positional_encoder.py
@@ -1,157 +1,76 @@
 import math
-from typing import Optional
 
 import torch
-from torch import nn
-
-from labml.logger import inspect
-from labml_nn.transformers.mha import MultiHeadAttention
+from torch import Tensor
 
 
-# Convert for Attention with Bidirectional Biases
-def get_slopes(n_heads: int):
-    n = 2 ** math.floor(math.log2(n_heads))
-    # $2^{-\frac{8}{n}}$
-    m_0 = 2.0 ** (-8.0 / n)
-    # $2^{-1\frac{8}{n}}, 2^{-2 \frac{8}{n}}, 2^{-3 \frac{8}{n}}, \dots$
-    m = torch.pow(m_0, torch.arange(1, 1 + n))
+class ALiBiBiEncoder:
+    def __init__(self):
+        pass
 
-    # If `n_heads` is not a power of 2, then we add the remaining slopes.
-    # We calculate the remaining slopes for $n * 2$ (avoiding slopes added previously).
-    # And pick the slopes upto `n_heads`.
-    if n < n_heads:
-        # $2^{-\frac{8}{2n}}$
-        m_hat_0 = 2.0 ** (-4.0 / n)
-        # $2^{-1\frac{8}{2n}}, 2^{-3 \frac{8}{2n}}, 2^{-5 \frac{8}{2n}}, \dots$
-        # Note that we take steps by $2$ to avoid slopes added previously.
-        m_hat = torch.pow(m_hat_0, torch.arange(1, 1 + 2 * (n_heads - n), 2))
-        # Concatenate the slopes with the remaining slopes.
-        m = torch.cat([m, m_hat])
+    @staticmethod
+    def get_slopes(n_heads: int):
+        n = 2 ** math.floor(math.log2(n_heads))
+        m_0 = 2.0 ** (-8.0 / n)
+        m = torch.pow(m_0, torch.arange(1, 1 + n, step=n / n_heads))
+        return m
 
-    return m
+    @staticmethod
+    def generate_reverse_distance_matrix(dimension: int, left_negative: True):
+        matrix = torch.zeros((dimension, dimension), dtype=torch.float)
+        multiplier = -1 if left_negative else 1
 
+        for i in range(dimension):
+            for j in range(dimension):
+                if i == j:
+                    matrix[i, j] = dimension
+                elif i < j:
+                    matrix[i, j] = dimension - abs(i - j)
+                elif i > j:
+                    matrix[i, j] = (dimension - abs(i - j)) * multiplier
 
-@torch.no_grad()
-def get_alibi_biases(n_heads: int, mask: torch.Tensor):
-    """
-    ## Calculate the attention biases matrix
+        return matrix
 
-    * `n_heads` is the number of heads in the attention layer
-    * `mask` is the attention mask of shape `[seq_len_q, seq_len_k]`
+    @staticmethod
+    def generate_distance_matrix(dim, with_mask=False):
+        matrix = torch.zeros(dim, dim)
 
-    This returns a matrix of shape `[seq_len_q, seq_len_k, n_heads, ]` with ALiBi attention biases.
-    """
+        for i in range(dim):
+            for j in range(dim):
+                if i == j:
+                    matrix[i, j] = 0
+                elif i < j:
+                    matrix[i, j] = 0 if with_mask else (j - i)
+                else:
+                    matrix[i, j] = i - j
+        return matrix
 
-    # Get slopes $m$ for each head
-    m = get_slopes(n_heads).to(mask.device)
-
-    # Calculate distances $[0, 1, \dots, N]$
-    # Here we calculate the distances using the mask.
-    #
-    # Since it's causal mask we can just use $[0, 1, \dots, N]$ too.
-    # `distance = torch.arange(mask.shape[1], dtype=torch.long, device=mask.device)[None, :]`
-    distance = mask.cumsum(dim=-1)
-
-    # Multiply them pair-wise to get the AliBi bias matrix
-    return distance[:, :, None] * m[None, None, :]
-
-
-class AlibiMultiHeadAttention(MultiHeadAttention):
-    """
-    ## Attention with Linear Biases (ALiBi)
-
-    We override [Multi-Head Attention](../mha.html).
-    """
-
-    def __init__(self, heads: int, d_model: int, dropout_prob: float = 0.1):
-        super().__init__(heads, d_model, dropout_prob)
-
-        # To cache AliBi the biases
-        self.alibi_biases = None
-
-    def forward(
-        self,
-        *,
-        query: torch.Tensor,
-        key: torch.Tensor,
-        value: torch.Tensor,
-        mask: Optional[torch.Tensor] = None
-    ):
+    @staticmethod
+    @torch.no_grad()
+    def get_alibi_biases(n_heads: int, sequence_length: int, with_mask=False, device="cpu") -> Tensor:
+        """ This bias will be subtracted from the real attention score
+        :param n_heads: Number of head in multi head attention.
+        :param sequence_length: Max supported sequence length in a sentence.
+        :param with_mask: Hide future words with mask or not.
+        :param device: device type.
+        :return: Bias tensor which will be subtracted from attention score.
         """
-        `query`, `key` and `value` are the tensors that store
-        collection of *query*, *key* and *value* vectors.
-        They have shape `[seq_len, batch_size, d_model]`.
+        m = ALiBiBiEncoder.get_slopes(n_heads).to(device)
+        distance_matrix = ALiBiBiEncoder.generate_distance_matrix(sequence_length, with_mask)
 
-        `mask` has shape `[seq_len, seq_len, batch_size]` and
-        `mask[i, j, b]` indicates whether for batch `b`,
-        query at position `i` has access to key-value at position `j`.
-        """
-
-        # ALiBi only works with causal masks.
-        assert mask is not None
-        assert mask.shape[0] == mask.shape[1] and mask.shape[2] == 1
-
-        # `query`, `key` and `value` have shape `[seq_len, batch_size, d_model]`
-        seq_len, batch_size, _ = query.shape
-
-        # Add head dimension to mask and check its shape.
-        mask = self.prepare_mask(mask, query.shape, key.shape)
-
-        # Prepare `query`, `key` and `value` for attention computation.
-        # These will then have shape `[seq_len, batch_size, heads, d_k]`.
-        query = self.query(query)
-        key = self.key(key)
-        value = self.value(value)
-
-        # Compute attention scores $Q K^\top$.
-        # This gives a tensor of shape `[seq_len, seq_len, batch_size, heads]`.
-        scores = self.get_scores(query, key)
-
-        # Scale scores $\frac{Q K^\top}{\sqrt{d_k}}$
-        scores *= self.scale
-
-        # Create AliBi biases if it's not cached
-        if self.alibi_biases is None or self.alibi_biases.shape[1] < seq_len:
-            # `mask` has shape [seq_len, seq_len, 1, 1]
-            self.alibi_biases = get_alibi_biases(scores.shape[-1], mask[:, :, 0, 0])
-
-        # Add AliBi biases to attention scores.
-        # ALiBi biases has shape `[seq_len, seq_len, n_heads]`
-        # and `scores` has shape `[seq_len, seq_len, batch_size, n_heads]`
-        scores += self.alibi_biases[:seq_len, :seq_len, None, :]
-
-        # Apply mask
-        scores = scores.masked_fill(mask == 0, float("-inf"))
-
-        # $softmax$ attention along the key sequence dimension
-        # $\underset{seq}{softmax}\Bigg(\frac{Q K^\top}{\sqrt{d_k}}\Bigg)$
-        attn = self.softmax(scores)
-
-        # Apply dropout
-        attn = self.dropout(attn)
-
-        # Multiply by values
-        # $$\underset{seq}{softmax}\Bigg(\frac{Q K^\top}{\sqrt{d_k}}\Bigg)V$$
-        x = torch.einsum("ijbh,jbhd->ibhd", attn, value)
-
-        # Concatenate multiple heads
-        x = x.reshape(seq_len, batch_size, -1)
-
-        # Output layer
-        return self.output(x)
+        # Multiply them pair-wise to get the AliBi bias matrix
+        return distance_matrix[:, :, None] * m[None, None, :]
 
 
 def _test_alibi():
-    """
-    Simple test function to see the slopes.
-    """
-    inspect(get_slopes(12).tolist(), _n=-1)
-    from labml_nn.transformers.utils import subsequent_mask
+    slopes = ALiBiBiEncoder.get_slopes(1)
+    print(f"slopes list for MHA {slopes}")
 
-    mask = subsequent_mask(8)[:, :, 0]
-    inspect(mask)
+    distance_matrix = ALiBiBiEncoder.generate_distance_matrix(5)
+    print(f"distance matrix {distance_matrix}")
 
-    inspect(get_alibi_biases(12, mask)[:, :, 3], _n=-1)
+    bias = ALiBiBiEncoder.get_alibi_biases(n_heads=8, sequence_length=10, with_mask=False)
+    print(f"bias shape {bias.shape} \n {bias}")
 
 
 if __name__ == "__main__":
