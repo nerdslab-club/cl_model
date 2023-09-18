6a5b4e8ddd19e26ba10d945197593f663538d503
afmjoaa <32086430+afmjoaa@users.noreply.github.com>
2023-09-19 00:25:43 +0600

    #5: Add save model using state dict function to all modules

--- cl_pretrainer/transformer.py
+++ cl_pretrainer/transformer.py
@@ -59,8 +59,12 @@ class Transformer(nn.Module):
     def save_model(self, path: str):
         torch.save(self.state_dict(), path)
 
-    def load_saved_model(self, path: str):
+    def load_saved_model_from_path(self, path: str):
         self.load_state_dict(torch.load(path))
+        self.eval()
+
+    def load_saved_model_from_state_dict(self, state_dict: dict):
+        self.load_state_dict(state_dict)
         self.eval()
 
 
