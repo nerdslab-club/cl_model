b9ae4d5710866d69bc50bb820969b12778d55e10
afmjoaa <32086430+afmjoaa@users.noreply.github.com>
2023-09-24 21:07:04 +0600

    #10: Add initial bolierplate for block classes

---
+++ cl_pretrainer/category_map_block.py
@@ -0,0 +1,21 @@
+import torch
+from torch import nn
+
+
+class CategoryMapBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x: torch.FloatTensor):
+        pass
+
+    def save_model(self, path: str):
+        torch.save(self.state_dict(), path)
+
+    def load_saved_model_from_path(self, path: str):
+        self.load_state_dict(torch.load(path))
+        self.eval()
+
+    def load_saved_model_from_state_dict(self, state_dict: dict):
+        self.load_state_dict(state_dict)
+        self.eval()
