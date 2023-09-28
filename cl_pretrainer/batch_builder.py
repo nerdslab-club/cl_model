36d2d542cfb2070e79a37707ccdfaaa42a1a1d14
afmjoaa <32086430+afmjoaa@users.noreply.github.com>
2023-09-29 03:22:19 +0600

    #9: Add initial batch builder functionality without mask

--- cl_pretrainer/batch_builder.py
+++ cl_pretrainer/batch_builder.py
@@ -2,48 +2,105 @@ from typing import Dict, List, Tuple, Optional
 
 import torch
 
+from cl_data.io_parser.io_parser import IoParser
+from cl_data.src.utility import Utility
+
+
 class BatchBuilder:
-    # 1. Pass each sentence of the corpus through io parser.
-    # 2. Take the input map add padding and special token and pass through get_batch_embedding_map
-    # 3. Create src padding mask and future mask
-    # 4. return the batch and mask map
-    SOURCE_LANGUAGE_KEY = "src"
-    TARGET_LANGUAGE_KEY = "tgt"
+    SOURCE_LANGUAGE_KEY = "inputStr"
+    TARGET_LANGUAGE_KEY = "outputStr"
 
     @staticmethod
-    def construct_batches(
+    def get_batch_io_parser_output(
+            sentences: list[str],
+            add_bos_and_eos: bool,
+            max_sequence_length: int,
+    ) -> list[list[dict]]:
+        """Given list of sentences it creates list of io parser output with special tokens and paddings
+
+        :param sentences: List of sentences.
+        :param add_bos_and_eos: Flag for weather to add BOS and EOS to the Token list.
+        :param max_sequence_length: Max length until which padding will be added. If None then no padding.
+        :return:
+        """
+        batch_io_parser_output = []
+        for sentence in sentences:
+            batch_io_parser_output.append(
+                BatchBuilder.get_sentence_io_parser_output(
+                    sentence,
+                    add_bos_and_eos,
+                    max_sequence_length,
+                ),
+            )
+        return batch_io_parser_output
+
+    @staticmethod
+    def get_sentence_io_parser_output(sentence: str, add_bos_and_eos: bool, max_sequence_length: int) -> list[dict]:
+        """Given a sentence it called io parser on the sentence with special tokens and paddings
+
+        :param sentence: A normal sentence string.
+        :param add_bos_and_eos: Flag for weather to add BOS and EOS to the Token list.
+        :param max_sequence_length: Max length until which padding will be added. If None then no padding.
+        :return:
+        """
+        io_parser_tuples = IoParser().create_value_list_from_input(sentence)
+        return Utility.create_io_map_from_io_tuple(
+            input_list=io_parser_tuples,
+            add_bos_and_eos=add_bos_and_eos,
+            max_length=max_sequence_length
+        )
+
+    @staticmethod
+    def construct_batches_for_transformer(
             corpus: List[Dict[str, str]],
             batch_size: int,
+            max_encoder_sequence_length: int,
+            max_decoder_sequence_length: int,
             device: Optional[torch.device] = None,
-    ) -> Tuple[Dict[str, List[torch.Tensor]], Dict[str, List[torch.Tensor]]]:
-        """ Constructs batches given a corpus.
+    ) -> Tuple[Dict[str, list[list[dict]]], Dict[str, List[torch.Tensor]]]:
+        """ Constructs batches given sample corpus.
 
         :param corpus: The input corpus is a list of aligned source and target sequences, packed in a dictionary.
+        ie.
+        [
+            {
+                    "inputStr": f"##factorial({x})",
+                    "outputStr": f"The factorial of {x}"
+            },
+            {
+                    "inputStr": f"##factorial({x})",
+                    "outputStr": f"The factorial of {x}"
+            }
+        ]
         :param batch_size: The number of sequences in a batch
+        :param max_encoder_sequence_length: Truncate/Allowed max encoder sequence length. If None then no padding.
+        :param max_decoder_sequence_length: Truncate/Allowed max decoder sequence length. If None then no padding.
         :param device: whether to move tensors to gpu
-        :return: A tuple containing two dictionaries. The first represents the batches, the second the attention masks.
+        :return: A tuple containing two dictionaries.
+        The first represents the batches, This is batch io parser output.
+        Second one represents the attention masks. This is bool Tensor.
         """
-        pad_token_id = vocab.token2index[vocab.PAD]
         batches: Dict[str, List] = {"src": [], "tgt": []}
         masks: Dict[str, List] = {"src": [], "tgt": []}
         for i in range(0, len(corpus), batch_size):
-            src_batch = torch.IntTensor(
-                vocab.batch_encode(
-                    [pair[src_lang_key] for pair in corpus[i: i + batch_size]],
-                    add_special_tokens=True,
-                    padding=True,
-                )
-            )
-            tgt_batch = torch.IntTensor(
-                vocab.batch_encode(
-                    [pair[tgt_lang_key] for pair in corpus[i: i + batch_size]],
-                    add_special_tokens=True,
-                    padding=True,
-                )
+            input_sentences = [pair.get(BatchBuilder.SOURCE_LANGUAGE_KEY, "") for pair in corpus[i: i + batch_size]]
+            src_batch = BatchBuilder.get_batch_io_parser_output(
+                sentences=input_sentences,
+                add_bos_and_eos=True,
+                max_sequence_length=max_encoder_sequence_length,
             )
 
-            src_padding_mask = src_batch != pad_token_id
-            future_mask = construct_future_mask(tgt_batch.shape[-1])
+            output_sentences = [pair.get(BatchBuilder.TARGET_LANGUAGE_KEY, "") for pair in corpus[i: i + batch_size]]
+            tgt_batch = BatchBuilder.get_batch_io_parser_output(
+                sentences=output_sentences,
+                add_bos_and_eos=True,
+                max_sequence_length=max_decoder_sequence_length,
+            )
+
+            # TODO fix the mask part
+            # suggestion : need to get the mask from get_batch_io_parser_output function as tuple
+            # src_padding_mask = src_batch != pad_token_id
+            # future_mask = construct_future_mask(tgt_batch.shape[-1])
 
             # Move tensors to gpu; if available
             if device is not None:
@@ -57,6 +114,7 @@ class BatchBuilder:
             masks["tgt"].append(future_mask)
         return batches, masks
 
+    @staticmethod
     def construct_future_mask(seq_len: int):
         """
         Construct a binary mask that contains 1's for all valid connections and 0's for all outgoing future connections.
