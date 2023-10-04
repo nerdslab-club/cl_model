9454df86d408b8b312503b46d7961a5b4118614a
afmjoaa <32086430+afmjoaa@users.noreply.github.com>
2023-10-04 14:38:13 +0600

    #9: Add test for simple vocabulary builder

--- vocabulary_builder/simple_vocabulary_builder.py
+++ vocabulary_builder/simple_vocabulary_builder.py
@@ -11,6 +11,7 @@ from cl_data.src.constants import (
     CategorySubSubType,
     Constants,
 )
+from cl_pretrainer.batch_builder import BatchBuilder
 
 
 @dataclass
@@ -219,80 +220,34 @@ class SimpleVocabBuilder:
                 CategorySubSubType.NONE.value,
             )
 
-# class TestVocabulary(unittest.TestCase):
-#     maxDiff = None
-#
-#     def test_tokenize(self):
-#         input_sequence = "Hello my name is Joris and I was born with the name Joris."
-#         output = Vocabulary([]).tokenize(input_sequence)
-#         self.assertEqual(
-#             [
-#                 "BOS",
-#                 "Hello",
-#                 "my",
-#                 "name",
-#                 "is",
-#                 "Joris",
-#                 "and",
-#                 "I",
-#                 "was",
-#                 "born",
-#                 "with",
-#                 "the",
-#                 "name",
-#                 "Joris",
-#                 ".",
-#                 "EOS",
-#             ],
-#             output,
-#         )
-#
-#     def test_init_vocab(self):
-#         input_sentences = ["Hello my name is Joris and I was born with the name Joris."]
-#         vocab = Vocabulary(input_sentences)
-#         expected = {
-#             "BOS": 0,
-#             "EOS": 1,
-#             "PAD": 2,
-#             "Hello": 3,
-#             "my": 4,
-#             "name": 5,
-#             "is": 6,
-#             "Joris": 7,
-#             "and": 8,
-#             "I": 9,
-#             "was": 10,
-#             "born": 11,
-#             "with": 12,
-#             "the": 13,
-#             ".": 14,
-#         }
-#         self.assertEqual(vocab.token2index, expected)
-#
-#     def test_encode(self):
-#         input_sentences = ["Hello my name is Joris and I was born with the name Joris."]
-#         vocab = Vocabulary(input_sentences)
-#         output = vocab.encode(input_sentences[0])
-#         self.assertEqual(output, [0, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 5, 7, 14, 1])
-#
-#     def test_encode_no_special_tokens(self):
-#         input_sentences = ["Hello my name is Joris and I was born with the name Joris."]
-#         vocab = Vocabulary(input_sentences)
-#         output = vocab.encode(input_sentences[0], add_special_tokens=False)
-#         self.assertEqual(output, [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 5, 7, 14])
-#
-#     def test_batch_encode(self):
-#         input_sentences = [
-#             "This is one sentence",
-#             "This is another, much longer sentence",
-#             "Short sentence",
-#         ]
-#         vocab = Vocabulary(input_sentences)
-#         output = vocab.batch_encode(input_sentences, add_special_tokens=False)
-#         self.assertEqual(
-#             output,
-#             [[3, 4, 5, 6, 2, 2, 2], [3, 4, 7, 8, 9, 10, 6], [11, 6, 2, 2, 2, 2, 2]],
-#         )
+
+class TestVocabulary(unittest.TestCase):
+    def test_init_vocab(self):
+        input_sentences = ["Hello my name is Joris and I was born with the name Joris."]
+        corpus_io_parser_output = BatchBuilder.get_batch_io_parser_output(input_sentences, True, 16)
+        vocab = SimpleVocabBuilder(corpus_io_parser_output)
+        print(vocab.vocab_item_to_index)
+        self.assertEqual(vocab.index_to_vocab_item[2], SimpleVocabBuilder.get_special_vocab_item(SpecialTokens.PADDING))
+        self.assertEqual(vocab.index_to_vocab_item[0], SimpleVocabBuilder.get_special_vocab_item(SpecialTokens.BEGINNING))
+        self.assertEqual(vocab.index_to_vocab_item[1], SimpleVocabBuilder.get_special_vocab_item(SpecialTokens.ENDING))
+        self.assertEqual(len(vocab.vocab_item_to_index.items()), 15)
+
+    def test_encode(self):
+        input_sequence = "Hello my name is Joris and I was born with the name Joris"
+        corpus = BatchBuilder.get_batch_io_parser_output([input_sequence], True, 16)
+        io_parser_output = BatchBuilder.get_sentence_io_parser_output(input_sequence, True, 18)
+
+        output = SimpleVocabBuilder(corpus).encoder(io_parser_output)
+        self.assertEqual(
+            output, [0, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 5, 7, 2, 2, 2, 1]
+        )
+
+    def test_decode(self):
+        input_sequence = "Hello my name is Joris and I was born with the name Joris"
+        corpus = BatchBuilder.get_batch_io_parser_output([input_sequence], True, 16)
+        decoded_output = SimpleVocabBuilder(corpus).decode([0, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 5, 7, 2, 2, 2, 1])
+        io_parser_output = BatchBuilder.get_sentence_io_parser_output(input_sequence, True, 18)
+        self.assertEqual(decoded_output, io_parser_output)
 
 
 if __name__ == "__main__":
