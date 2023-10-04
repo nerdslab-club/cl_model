2de9331ab4a13abbde89ad3507510f39d6d79811
afmjoaa <32086430+afmjoaa@users.noreply.github.com>
2023-10-04 14:01:41 +0600

    #9: Remove unused transformer util and vocabulary class

--- vocabulary_builder/simple_vocabulary_builder.py
+++ vocabulary_builder/simple_vocabulary_builder.py
@@ -1,3 +1,4 @@
+import unittest
 from dataclasses import dataclass
 from typing import Optional
 
@@ -218,6 +219,81 @@ class SimpleVocabBuilder:
                 CategorySubSubType.NONE.value,
             )
 
+# class TestVocabulary(unittest.TestCase):
+#     maxDiff = None
+#
+#     def test_tokenize(self):
+#         input_sequence = "Hello my name is Joris and I was born with the name Joris."
+#         output = Vocabulary([]).tokenize(input_sequence)
+#         self.assertEqual(
+#             [
+#                 "BOS",
+#                 "Hello",
+#                 "my",
+#                 "name",
+#                 "is",
+#                 "Joris",
+#                 "and",
+#                 "I",
+#                 "was",
+#                 "born",
+#                 "with",
+#                 "the",
+#                 "name",
+#                 "Joris",
+#                 ".",
+#                 "EOS",
+#             ],
+#             output,
+#         )
+#
+#     def test_init_vocab(self):
+#         input_sentences = ["Hello my name is Joris and I was born with the name Joris."]
+#         vocab = Vocabulary(input_sentences)
+#         expected = {
+#             "BOS": 0,
+#             "EOS": 1,
+#             "PAD": 2,
+#             "Hello": 3,
+#             "my": 4,
+#             "name": 5,
+#             "is": 6,
+#             "Joris": 7,
+#             "and": 8,
+#             "I": 9,
+#             "was": 10,
+#             "born": 11,
+#             "with": 12,
+#             "the": 13,
+#             ".": 14,
+#         }
+#         self.assertEqual(vocab.token2index, expected)
+#
+#     def test_encode(self):
+#         input_sentences = ["Hello my name is Joris and I was born with the name Joris."]
+#         vocab = Vocabulary(input_sentences)
+#         output = vocab.encode(input_sentences[0])
+#         self.assertEqual(output, [0, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 5, 7, 14, 1])
+#
+#     def test_encode_no_special_tokens(self):
+#         input_sentences = ["Hello my name is Joris and I was born with the name Joris."]
+#         vocab = Vocabulary(input_sentences)
+#         output = vocab.encode(input_sentences[0], add_special_tokens=False)
+#         self.assertEqual(output, [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 5, 7, 14])
+#
+#     def test_batch_encode(self):
+#         input_sentences = [
+#             "This is one sentence",
+#             "This is another, much longer sentence",
+#             "Short sentence",
+#         ]
+#         vocab = Vocabulary(input_sentences)
+#         output = vocab.batch_encode(input_sentences, add_special_tokens=False)
+#         self.assertEqual(
+#             output,
+#             [[3, 4, 5, 6, 2, 2, 2], [3, 4, 7, 8, 9, 10, 6], [11, 6, 2, 2, 2, 2, 2]],
+#         )
+
 
 if __name__ == "__main__":
     vocab_item_one = VocabItem(10, "bool", "default", "param_one")
@@ -226,3 +302,5 @@ if __name__ == "__main__":
 
     print(vocab_item_one == vocab_item_two)
     print(vocab_item_one == vocab_item_three)
+    unittest.main()
+
