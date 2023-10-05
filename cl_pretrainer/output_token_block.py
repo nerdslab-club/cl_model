ecb297f88f6fc41fc894d7085aac68cc3ff879e7
afmjoaa <32086430+afmjoaa@users.noreply.github.com>
2023-10-05 17:55:01 +0600

    #10: Implement output token block class

--- cl_pretrainer/output_token_block.py
+++ cl_pretrainer/output_token_block.py
@@ -7,25 +7,34 @@ from cl_pretrainer.multi_head_attention import MultiHeadAttention
 class OutputTokenBlock(nn.Module):
     def __init__(self, hidden_dim: int, ff_dim: int, num_heads: int, dropout_p: float):
         super().__init__()
-        self.self_mha = MultiHeadAttention(hidden_dim, num_heads)
-        self.feed_forward = nn.Sequential(
+        self.output_token_block_self_mha = MultiHeadAttention(hidden_dim, num_heads)
+        self.output_token_block_feed_forward = nn.Sequential(
             nn.Linear(hidden_dim, ff_dim),
             nn.LeakyReLU(),
             nn.Linear(ff_dim, hidden_dim),
         )
         # Dropout is also known as regularization
-        self.dropout1 = nn.Dropout(p=dropout_p)
-        self.dropout2 = nn.Dropout(p=dropout_p)
+        self.output_token_block_dropout1 = nn.Dropout(p=dropout_p)
+        self.output_token_block_dropout2 = nn.Dropout(p=dropout_p)
         # Normalizing layer for propagating the token values
-        self.layer_norm1 = nn.LayerNorm(hidden_dim)
-        self.layer_norm2 = nn.LayerNorm(hidden_dim)
-        pass
+        self.output_token_block_layer_norm1 = nn.LayerNorm(hidden_dim)
+        self.output_token_block_layer_norm2 = nn.LayerNorm(hidden_dim)
 
     def forward(self, x: torch.FloatTensor):
-        # x -> input embeddings
-        # src padding mask
-        # future mask
-        pass
+        """ output token block takes the x tensor which is the output of the common block.
+        As common block is using both padding mask and future mask we are not using any mask in this layer.
+
+        :param x: The output of the common block.
+        :return: The embeddings for the output token -> E2.
+        """
+        # Multi head attention layer
+        output = self.output_token_block_dropout1(self.output_token_block_self_mha.forward(x))
+        x = self.output_token_block_layer_norm1(x + output)
+
+        # Feed forward layers
+        output = self.output_token_block_dropout2(self.output_token_block_feed_forward(x))
+        x = self.output_token_block_layer_norm2(x + output)
+        return x
 
     def save_model(self, path: str):
         torch.save(self.state_dict(), path)
