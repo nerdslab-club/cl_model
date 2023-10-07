702111f731733f06bbcb1876afb6a6444f20257b
afmjoaa <32086430+afmjoaa@users.noreply.github.com>
2023-10-07 16:07:44 +0600

    #10: Add output token decoder test

--- cl_pretrainer/output_token_decoder.py
+++ cl_pretrainer/output_token_decoder.py
@@ -1,10 +1,14 @@
 import math
+import unittest
 from typing import Optional
 
 import torch
 from torch import nn
 from torch.nn.init import xavier_uniform_
 
+from cl_data.src.constants import TaskTypes
+from cl_pretrainer.batch_builder import BatchBuilder
+from cl_pretrainer.category_map_decoder import CategoryMapDecoder
 from cl_pretrainer.common_block import CommonBlock
 from cl_pretrainer.output_token_block import OutputTokenBlock
 from embeddings_manager.embeddings_manager import EmbeddingsManager
@@ -87,3 +91,60 @@ class OutputTokenDecoder(nn.Module):
     def load_saved_model_from_state_dict(self, state_dict: dict):
         self.load_state_dict(state_dict)
         self.eval()
+
+
+class TestCategoryMapDecoder(unittest.TestCase):
+
+    def test_output_token_decoder(self):
+        """
+        Test three forward pass of output token decoder and check if output tensor shape is as expected.
+        :return: None
+        """
+        batch_size = 2
+        num_heads = 8
+        max_decoding_length = 10
+        hidden_dim = 768
+        ff_dim = 2048
+        num_layers = 2
+        dropout_p = 0.1
+        task_type = TaskTypes.NL_TO_NL_TRANSLATION.value
+        sentences = [
+            "Hello my name is Joaa and my parents gave the name Joaa.",
+            "Hello my name is Prattoy and my grandma gave the name Prattoy."
+        ]
+        with torch.no_grad():
+            output_token_decoder = OutputTokenDecoder(
+                embeddings_manager=EmbeddingsManager(
+                    batch_size=batch_size,
+                    n_heads=num_heads,
+                    max_sequence_length=max_decoding_length,
+                    with_mask=False,
+                ),
+                hidden_dim=hidden_dim,
+                ff_dim=ff_dim,
+                num_heads=num_heads,
+                num_layers=num_layers,
+                dropout_p=dropout_p,
+            )
+            output_token_decoder._reset_parameters()
+            output_token_decoder.eval()
+
+            batch_io_parser = BatchBuilder.get_batch_io_parser_output(sentences, True, 1)
+            future_mask = BatchBuilder.construct_future_mask(1)
+            for i in range(3):
+                index = i + 1
+                output_token_decoder_output = output_token_decoder.forward(
+                    batch_io_parser,
+                    task_type,
+                    future_mask=future_mask,
+                )
+
+                # Teacher forcing
+                batch_io_parser = BatchBuilder.get_batch_io_parser_output(sentences, True, index + 1)
+                future_mask = BatchBuilder.construct_future_mask(index + 1)
+
+                self.assertEqual(output_token_decoder_output.shape, (batch_size, index, hidden_dim))
+
+
+if __name__ == "__main__":
+    unittest.main()
