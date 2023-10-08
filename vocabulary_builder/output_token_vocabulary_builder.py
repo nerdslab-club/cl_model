4c95cd5ed2f70c87c2bed22c8d93104f75d67302
afmjoaa <32086430+afmjoaa@users.noreply.github.com>
2023-10-08 16:37:29 +0600

    #11: Implement output token classification head vocab item in the category vocabulary builder

--- vocabulary_builder/output_token_vocabulary_builder.py
+++ vocabulary_builder/output_token_vocabulary_builder.py
@@ -1,1 +1,31 @@
 # create multiple vocabulary for token based on unique category type and subtype.
+from dataclasses import dataclass
+
+
+@dataclass
+class OutputTokenVocabItem:
+    token: any
+
+    def __hash__(self):
+        """
+        Calculate a hash value based on the fields that determine equality
+
+        :return: The calculated hash
+        """
+        return hash(
+            (
+                self.token,
+            )
+        )
+
+    def __str__(self):
+        return f"OutputTokenVocabItem( token={self.token} )"
+
+
+class OutputTokenVocabBuilder:
+    """
+    integer token -> Category vocab item -> Category map
+    """
+    def __init__(self):
+        pass
+
