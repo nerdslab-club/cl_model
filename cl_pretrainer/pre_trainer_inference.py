cbdc1b420d90de0544fa0d23fcebca606ca8e834
afmjoaa <32086430+afmjoaa@users.noreply.github.com>
2023-10-11 01:31:19 +0600

    #13: Add cl pre-trainer train and inference method which is to be implemented

---
+++ cl_pretrainer/pre_trainer_inference.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+def cl_pre_trainer_inference():
+    pass
+
+
+class TestClPreTrainerInference(unittest.TestCase):
+
+    def test_cl_pre_trainer_model_load_and_inference(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ cl_pretrainer/pre_trainer_train.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+def cl_pre_trainer_train():
+    pass
+
+
+class TestClPreTrainerTraining(unittest.TestCase):
+
+    def test_cl_pre_trainer_train_and_save(self):
+        pass
+
+    def test_cl_pre_trainer_model_load(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
